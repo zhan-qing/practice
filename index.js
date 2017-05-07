22424d2b4679354a3d298e84725274c934c42d42
zhan-qing <b03303091@ntu.edu.tw>
2017-05-08 00:09:34 +0800

    new index

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('#NTUinvi').modal('fast');
+  $('#collection-3').modal('fast');
+  $('#sketch').modal('fast');
+  $('#worklink').scrollspy({target: '#collections'});
+ 
+});
+
