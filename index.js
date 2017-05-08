04d5a86dfc1aac0d9a428a8e450f5dd2226373ab
zhan-qing <b03303091@ntu.edu.tw>
2017-05-08 22:27:58 +0800

    index new

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ $(function () {
   $('#worklink').click(function(){
     var Height = $('#collections').offset().top;
     $('body').animate( {scrollTop: Height}, 500);
-  })
+  });
   $('#weblink').click(function(){
     var Height = $('#web').offset().top;
     $('body').animate({scrollTop: Height}, 500);
-  })
+  });
   $('#applink').click(function(){
     var Height = $('#app').offset().top;
     $('body').animate({scrollTop: Height}, 500);
-  })
+  });
+  $('#backlink').click(function(){
+    $('body').animate({scrollTop: 0}, 500);
+  });
+
+  $(window).on('scroll', function() {
+    if ($('body').scrollTop() == 0){
+      $('#backlink').fadeOut('fast');
+    }
+    else {
+      $('#backlink').fadeIn('fast');
+    }
+  });
 });
 
+  
